ce7f5c28e3729e16abec7fc4d462edfc1b430f19
Heva <he.wu@linecorp.com>
2019-07-02 16:35:26 +0900

    Create TestColorAssets Project

---
+++ TestColorAssets/TestColorAssets/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestColorAssets
+//
+//  Created by ST21235 on 2019/07/02.
+//  Copyright © 2019 He Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
