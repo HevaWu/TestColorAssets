da10ef2b47f4a7fd4bd2bd32cb6cf10bddb08991
Heva <he.wu@linecorp.com>
2019-07-03 14:26:37 +0900

    Add more comments

--- TestColorAssets/TestColorAssets/FirstViewController.swift
+++ TestColorAssets/TestColorAssets/FirstViewController.swift
@@ -15,17 +15,54 @@ class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        print(firstLabel.tintColor)
+        
+        // not use `Named Color` at xib file, we could rewrite the color at where we want
+        
+        // used `Named Color` at xib file, and try to rewrite it in the view controller
+        // Note: should pay attention to rewrite it at viewDidLoad()
+        
+        // if we rewrite the color in the viewDidLoad
+        // output:
+        // Optional(UIExtendedSRGBColorSpace 1 0 0 1) <- we rewrite the color
+        // Optional(kCGColorSpaceModelRGB 0 0 0 0.5 ) 31 <- it read the `named` color from xib file after `viewDidLoad`
 //        firstLabel.textColor = .red
         print(firstLabel.textColor)
+        
+        // observer for checking if the color is changed
         observer = firstLabel.observe(\.textColor) { (label, change) in
             print(label.textColor, #line)
         }
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // output:
+        // Optional(UIExtendedSRGBColorSpace 0.776 0.349 0.282 0.5)  <- first read `Named Color` set at viewDidLoad
+        // Optional(kCGColorSpaceModelRGB 0 0 0 0.5 ) 33  <- iPhone XR device read the `display3` color
+        // Optional(UIExtendedSRGBColorSpace 1 0 0 1) 33  <- rewrite color successful
+        firstLabel.textColor = .red
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // output:
+        // Optional(UIExtendedSRGBColorSpace 0.776 0.349 0.282 0.5) <- first read `Named Color` set at viewDidLoad
+        // Optional(UIExtendedSRGBColorSpace 1 0 0 1) 33 <- rewrite color
+        // Optional(kCGColorSpaceModelRGB 0 0 0 0.5 ) 33 <- iPhone XR device read the `display3` color
+//        firstLabel.textColor = .red
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        // if we rewrite the color in the viewDidAppear
+        // output:
+        // Optional(UIExtendedSRGBColorSpace 0.776 0.349 0.282 0.5)  <- first read `Named Color` set at viewDidLoad
+        // Optional(kCGColorSpaceModelRGB 0 0 0 0.5 ) 33  <- iPhone XR device read the `display3` color
+        // Optional(UIExtendedSRGBColorSpace 1 0 0 1) 33  <- rewrite color successful
+//        firstLabel.textColor = .red
     }
 }
 
